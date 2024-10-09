35f12573d06c8c24b9ad35712196c6ec77136a47
Ritesh <hiranwaritesh@gmail.com>
2024-10-10 01:16:40 +0530

    job controller

--- controllers/job.controller.js
+++ controllers/job.controller.js
@@ -83,7 +83,17 @@ export const getJobById = async (req,res) => {
 export const getAdminJobs = async (req,res) => {
     try {
         const adminId = req.id;
-        const jobs = await Job.find({created_by:adminId})
+        const jobs = await Job.find({created_by:adminId});
+        if(!jobs){
+            return res.status(404).json({
+                message:"Job not found.",
+                success:false
+            })
+        };
+        return res.status(200).json({
+            jobs,
+            success:true
+        })
     } catch (error) {
         console.log(error);
         
