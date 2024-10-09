f8962faacd6b17d8fc25c3595e8d6d5de7ca5400
Ritesh <hiranwaritesh@gmail.com>
2024-10-10 02:23:29 +0530

    API routes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
 
 dotenv.config({});
 
@@ -20,9 +22,13 @@ const corsOption = {
 
 app.use(cors(corsOption));
 
+
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+
 
 
 app.listen(PORT,()=>{
