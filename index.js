2cb4de536ab59210ce0d41e13a06125971008015
Ritesh <hiranwaritesh@gmail.com>
2024-10-10 00:18:49 +0530

    create companycontroller

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
 
 dotenv.config({});
 
@@ -20,9 +21,11 @@ const corsOption = {
 
 app.use(cors(corsOption));
 
+
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
 
 
 app.listen(PORT,()=>{
