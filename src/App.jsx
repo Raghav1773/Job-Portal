d1781eeafb5a9dbd4be1cb4c146d6331909e6c4c
Raghav1773 <raghavsharmaa1773@gmail.com>
2024-10-09 20:24:26 +0530

    added new files

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Signup from './components/auth/Signup'
+import Login from './components/auth/Login'
+import Home from './components/Home'
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/Login',
+    element:<Login/>
+  },
+  {
+    path:'/Signup',
+    element:<Signup/>
+  },
+])
+function App() {
+  
+  return (
+    <div>
+      <RouterProvider router = {appRouter}/>
+    </div>
+  )
+}
+
+export default App
